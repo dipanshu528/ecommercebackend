916f809f9279b5fad47eedfe5bf1544a76ab4cfb
dipanshu528 <vermadipanshu528@gmail.com>
2023-10-14 00:10:39 +0530

    changes

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ require("dotenv").config();
 const app = express();
 const PORT =  process.env.PORT || 6010 ;
 
- app.use(cors());
+ app.use(cors({
+    origin: "http://localhost:3000",
+  methods: ["GET","POST", "DELETE"],  
+  credentials: true
+ }
+ ));
 
 app.use(express.json())
 
